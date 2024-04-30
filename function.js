8ae3b1af1c71509f31261525eb7667d5c2eb2a74
mdruhulkuddus <ruhul15-14171@diu.edu.bd>
2024-04-30 20:15:26 +0600

    function and set

---
+++ function.js
@@ -0,0 +1,14 @@
+// named function
+
+function greet(name){
+    return 'Hello, ' + name + ' !';
+}
+console.log(greet('RUhul'));
+
+
+// anonymous function
+
+let greet = function(name){
+    return 'Hello, ' + name + '!';
+}
+console.log(greet('kuddus'));
---
+++ sets.js
@@ -0,0 +1,15 @@
+const letters = new Set(["a","b", "c"]);
+
+console.log(letters);
+
+letters.add(2);
+letters.add('o');
+
+console.log(letters);
+
+const numbers = new Set();
+numbers.add(4);
+numbers.add(4);
+numbers.add(6);
+
+console.log(numbers);
