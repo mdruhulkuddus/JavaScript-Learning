a722efa2ba677dad42d99108e0530b7ab23f28ff
mdruhulkuddus <ruhul15-14171@diu.edu.bd>
2024-05-08 19:52:07 +0600

    dom hands on

---
+++ DOM/script.js
@@ -0,0 +1,30 @@
+//  console.dir(document);
+// console.log(typeof(document));
+// console.log(document.title);
+// console.log(document.head);
+// console.log(typeof(document.head));
+
+// document.title = "here change";
+// console.log(document.title);
+// // console.dir(document.head);
+// console.log(document.all[5]);
+// console.log(document.links);
+
+// console.log(document.getElementById('header'));
+
+let headerELement = document.getElementById('header'); // object
+headerELement.textContent = "DOM- document object Model";
+// headerELement.innerText = "DOM- document object Model"; 
+
+// console.log(headerELement.textContent);
+// console.log(headerELement.innerText);
+// console.log(headerELement.innerHTML);
+
+// select by 
+// console.log(headerELement);
+// headerELement.style.fontSize = '50px';
+
+console.log(document.getElementsByClassName('htag'));
+
+let items = document.getElementsByTagName('h1');
+console.log(items);
--- incrementApp-Vanila-React/react.js
+++ incrementApp-Vanila-React/react.js
@@ -1,7 +1,7 @@
 // console.log(React);
 // console.log(ReactDOM);
 
- const domContainer = document.querySelector("#root");
+const domContainer = document.querySelector("#root");
 
 // const myElement = React.createElement("div", null, "Hellow wordl");
 
@@ -13,20 +13,26 @@
 // p.innerHTML = "hellow world";
 // domContainer.appendChild(p);
 
-const Increment = () =>{
-    const [counter, setcounter] = React.useState(0);
-    
-   return (
+const Increment = () => {
+    const [counter, setCounter] = React.useState(0);
+    return (
         <div>
-                <h1 id="display" class="m-5" >{counter}</h1>
-           <div>
-           <button type="button" id="button" class="btn btn-success" onClick={setcounter(counter+1)}>Increment + </button>
-           </div>
+            <h1 id="display" class="m-5" >{ counter}</h1>
+            <div>
+                <button type="button" id="button" class="btn btn-success" onClick={() => setCounter(counter+1)}  >Increment + </button>
+            </div>
         </div>
     );
 };
 
 // ReactDOM.render(Increment(), domContainer);
-ReactDOM.render(<Increment />, domContainer);
+ReactDOM.render(
+<div className="container">
+    <Increment />
+    <Increment />
+    <Increment />
+</div>,
+
+domContainer);
 
 
