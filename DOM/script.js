7168ff5a9c5abbdf3602040c7496eb119a4a0c6f
mdruhulkuddus <ruhul15-14171@diu.edu.bd>
2024-05-09 20:12:59 +0600

    dom

--- DOM/script.js
+++ DOM/script.js
@@ -14,17 +14,36 @@
 
 let headerELement = document.getElementById('header'); // object
 headerELement.textContent = "DOM- document object Model";
-// headerELement.innerText = "DOM- document object Model"; 
+// // headerELement.innerText = "DOM- document object Model"; 
 
-// console.log(headerELement.textContent);
-// console.log(headerELement.innerText);
-// console.log(headerELement.innerHTML);
+// // console.log(headerELement.textContent);
+// // console.log(headerELement.innerText);
+// // console.log(headerELement.innerHTML);
 
-// select by 
-// console.log(headerELement);
-// headerELement.style.fontSize = '50px';
+// // select by 
+// // console.log(headerELement);
+// // headerELement.style.fontSize = '50px';
 
-console.log(document.getElementsByClassName('htag'));
+// console.log(document.getElementsByClassName('htag'));
 
-let items = document.getElementsByTagName('h1');
-console.log(items);
+// let items = document.getElementsByTagName('h1');
+// console.log(items);
+
+// parent/child relation
+// const grandParent = document.querySelector('.list');
+// const parent = grandParent.children;
+// const child1 = parent[0].children;
+// const child = parent[1].children;
+
+// const childrenOne = document.querySelector('.list');
+// const childrenTwo = childrenOne.nextElementSibling;
+// childrenTwo.style.color = 'red';
+// console.log(childrenTwo);
+
+const divElement = document.createElement('div');
+divElement.className = "javascript";
+divElement.setAttribute('id', 'js');
+divElement.setAttribute('title', 'red div');
+
+
+
